fb3e01fba65ddc0dd973cfc7a9e387a2e5f0e8bc
Hawtian Wang <twistoy.wang@gmail.com>
2024-11-16 22:21:33 +0800

    Try nil language server

--- src/nix.rs
+++ src/nix.rs
@@ -1,6 +1,8 @@
-use zed_extension_api::{self as zed, LanguageServerId, Result};
+use zed_extension_api::{self as zed, settings::LspSettings, LanguageServerId, Result};
 
 struct NixExtension;
+
+const LSP_SERVERS: &[&'static str] = &["nixd", "nil"];
 
 impl zed::Extension for NixExtension {
     fn new() -> Self {
@@ -12,17 +14,70 @@ impl zed::Extension for NixExtension {
         _: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let path = worktree.which("nixd").ok_or_else(|| {
-            "The Nix language server (nixd) is not available in your environment (PATH).
-                You can install it from https://github.com/nix-community/nixd."
-                .to_string()
-        })?;
+        // returns the first available language server
+        let path = LSP_SERVERS
+            .iter()
+            .find_map(|binary_name| worktree.which(binary_name))
+            .ok_or_else(|| {
+                "No Nix language server (nixd or nil) is available in your environment (PATH)."
+                    .to_string()
+            })?;
 
         Ok(zed::Command {
             command: path,
             args: vec![],
             env: vec![],
         })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree);
+        match settings {
+            Ok(settings) => {
+                let init_opts = settings.initialization_options;
+                match init_opts {
+                    Some(init_opts) => {
+                        return Ok(Some(zed::serde_json::to_value(init_opts).map_err(
+                            |_| {
+                                "Failed to serialize LSP initialization options to JSON."
+                                    .to_string()
+                            },
+                        )?));
+                    }
+                    None => Ok(None),
+                }
+            }
+            Err(_err) => Ok(None),
+        }
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree);
+        match settings {
+            Ok(settings) => {
+                let settings = settings.settings;
+                match settings {
+                    Some(init_opts) => {
+                        return Ok(Some(zed::serde_json::to_value(init_opts).map_err(
+                            |_| {
+                                "Failed to serialize LSP initialization options to JSON."
+                                    .to_string()
+                            },
+                        )?));
+                    }
+                    None => Ok(None),
+                }
+            }
+            Err(_err) => Ok(None),
+        }
     }
 }
 
